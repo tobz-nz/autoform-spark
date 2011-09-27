93d2ef034aa70885f2288fae37ff29b1c83712d6
Toby Evans <tobz.nz@gmail.com>
2011-09-28 11:07:12 +1300

    add a missing function that wasn't ported over.

--- libraries/autoform.php
+++ libraries/autoform.php
@@ -883,6 +883,17 @@ class Autoform {
       return 'text';
     }
   }
+
+  private function object_to_array($data) {
+    if(is_array($data) || is_object($data)) {
+      $result = array(); 
+      foreach($data as $key => $value) { 
+        $result[$key] = object_to_array($value); 
+      }
+      return $result;
+    }
+    return $data;
+  }
   
   /**
    * Reset the entire form

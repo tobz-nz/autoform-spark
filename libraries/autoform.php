49761db5494408745f14e1fa9b6384c882f3a838
Toby Evans <tobz.nz@gmail.com>
2011-11-17 13:38:24 +1300

    fix bug with re-populating checkboxes

--- libraries/autoform.php
+++ libraries/autoform.php
@@ -667,7 +667,7 @@ class Autoform {
     }
     
     // set checked status
-    $field->checked = ($this->CI->input->post($field->name) == $field->value ? true : false);
+    $field->checked = (isset($_POST[$field->name]) && $this->CI->input->post($field->name) == $field->value ? true : false);
     
     // set checked status on posted array (fieldname[])
 		$raw_name = preg_replace('/^(.{0,})(\[\])$/', '$1', $field->name); // remove ending []
